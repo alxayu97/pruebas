0cb4efecb86684ddd5f14e480708b168530ff506
aayuso <aayuso@I1-5>
2019-01-31 12:53:07 +0100

    Cuarto commit

--- index.php
+++ index.php
@@ -16,7 +16,6 @@ and open the template in the editor.
         echo "Voy a guardar el primer Commit";
         echo "Voy a hacer el segundo Commit";
         echo "Voy a hacer el tercer Commit";
-        echo "Voy a hacer el cuarto Commit";
         ?>
     </body>
 </html>
